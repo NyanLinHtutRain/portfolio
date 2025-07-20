f977e97d568581782dcc3a416e549f77c432e57b
NyanLinHtutRain <acidr37@gmail.com>
2025-07-20 18:13:11 +0800

    Add LangGraph, LangChain, JPMorgan, GrabFin, Testamur certs and navbar link

--- pages/certificates.js
+++ pages/certificates.js
@@ -9,6 +9,14 @@ import thumbSolutionArchitect from '../public/images/contents/solutionarchitect.
 import thumbHackerRank from '../public/images/contents/hackerrank.jpg'
 import thumbUdemySwift from '../public/images/contents/CertificateUdemySwift_APPdevlopmentNYANLINHTUT.jpg'
 import thumbMdp1stPrize from '../public/images/contents/MDP_1stprize_certificate.jpeg'
+import thumbLangGraph from '../public/images/contents/DeeplearningAI.png'
+import thumbLangChain from '../public/images/contents/DeeplearningAI.png'
+import thumbJPMorgan from '../public/images/contents/jpmorgan_work_simulation.jpg'
+import thumbGrabFin from '../public/images/contents/grabfin_marketing_simulation.png'
+import thumbTestamur from '../public/images/contents/testamur.jpg'
+
+
+
 
 
 
@@ -34,14 +42,33 @@ const Certificates = () => (
           <GridItem
             title=""
             thumbnail={thumbSolutionArchitect}
-            href="https://www.youtube.com/watch?v=KKxhf50FIPI"
+            href="https://www.credly.com/badges/2d6ae320-81df-41f1-9541-f7bd003e7aac/public_url"
           />
-    
+          <GridItem
+            title="AI Agents in LangGraph – DeepLearning.AI"
+            thumbnail={thumbLangGraph}
+            href="https://learn.deeplearning.ai/accomplishments/50255d84-26c4-43c7-b186-49f9a59cf01a?usp=sharing"
+          />
+          <GridItem
+            title="LangChain for LLM Application Development – DeepLearning.AI"
+            thumbnail={thumbLangChain}
+            href="https://learn.deeplearning.ai/accomplishments/b0f829c0-063a-4e16-aea9-4fcf9435da77?usp=sharing"
+          />
         </SimpleGrid>
       </Section>
 
       <Section delay={0.3}>
         <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            title="JPMorgan Chase Software Engineering – Work Simulation Project"
+            thumbnail={thumbJPMorgan}
+            href="https://www.theforage.com/simulations/jpmorgan/advanced-software-engineering-r0fm"
+          />
+          <GridItem
+            title="GrabFin – On-Demand Marketing Strategy Simulation"
+            thumbnail={thumbGrabFin}
+            href="https://www.talentgeist.com/worksimulationprogram"
+          />
           <GridItem
             title="Software Engineer Intern Certificate"
             thumbnail={thumbHackerRank}
@@ -57,7 +84,11 @@ const Certificates = () => (
             thumbnail={thumbMdp1stPrize}
             href="/images/contents/MDP_1stprize_certificate.jpeg"
           />
-
+          <GridItem
+            title="Graduate Bachelor in Information Technology – Testamur"
+            thumbnail={thumbTestamur}
+            href="/images/contents/testamur.jpg"
+          />
         </SimpleGrid> 
       </Section>
 
